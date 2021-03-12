7776d109b5c2c500136b7260fb3fdfb992c04fdc
samibarmil <77699305+samibarmil@users.noreply.github.com>
2021-03-11 23:08:56 -0600

    upload ascii homework

---
+++ ascii/ascii.js
@@ -0,0 +1,111 @@
+let  playing_speed = "250";
+let isPlaying = true;
+
+window.onload = function () {
+    btnplay.onclick = play_anime;
+    btnstop.onclick = stop_anime;
+    ddlanimation.onchange = anime_content;
+    ddlsize.onchange = change_size;
+    ckbturbo.onchange = change_speed;
+    change_size();
+    change_speed();
+    set_playing(false);
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function set_playing(status) {
+    if (status == true) {
+        isPlaying = true;
+        btnplay.disabled = true;
+        btnstop.disabled = false;
+        ddlanimation.disabled = true;
+    }
+    else {
+        isPlaying = false;
+        btnplay.disabled = false;
+        btnstop.disabled = true;
+        ddlanimation.disabled = false;
+    }
+}
+
+async function play_anime() {
+    set_playing(true);
+    let frames = anime_content().split("=====\n");
+    console.log("start playing");
+    while (isPlaying) {
+        for (frame of frames) {
+            txtdisplay.value = frame;
+            await sleep(playing_speed);
+            if(!isPlaying)
+                break;
+        }
+    }
+}
+
+function stop_anime() {
+    set_playing(false);
+    console.log("stop playing");
+}
+
+function change_speed() { 
+    console.log("changing speed");
+    if(ckbturbo.checked == true) {
+        playing_speed = 50;
+    }
+    else playing_speed = 250;
+}
+
+function change_size() {
+    console.log("changing size");
+    let tiny = "7pt";
+    let small = "10pt";
+    let medium = "12pt";let large = "16pt";
+    let extra_large = "24pt";
+    let xxl = "32pt";
+
+    switch (ddlsize.value) {
+        case "tiny":
+            console.log("tiny is choosen");
+            txtdisplay.style.fontSize = tiny;
+            break;
+        case "small":
+            txtdisplay.style.fontSize = small;
+            break;
+        case "medium":
+            txtdisplay.style.fontSize = medium;
+            break;
+        case "large":
+            txtdisplay.style.fontSize = large;
+            break;
+        case "extra_large":
+            txtdisplay.style.fontSize = extra_large;
+            break;
+        case "xxl":
+            txtdisplay.style.fontSize = xxl;
+    }
+}
+
+function anime_content() {
+    switch (ddlanimation.value) {
+        case "blank":
+            console.log("blank is choosen");
+            txtdisplay.value = BLANK;
+            break;
+        case "exercise":
+            txtdisplay.value = EXERCISE;
+            break;
+        case "juggler":
+            txtdisplay.value = JUGGLER;
+            break;
+        case "bike":
+            txtdisplay.value = BIKE;
+            break;
+        case "dive":
+            txtdisplay.value = DIVE;
+    }
+
+    return txtdisplay.value;
+}
--- textDecorator/decorate.js
+++ textDecorator/decorate.js
@@ -1,5 +1,8 @@
 
-
+window.onload = function () {
+    btnbigger.onclick = biggerText;
+    ckbboldit.onchange = boldit;
+}
 
 function test() {
 
@@ -9,55 +12,38 @@ function test() {
 }
 
 function biggerText() {
-    let usrText = document.getElementById("text");
-    console.log(usrText.style.fontSize);
-    usrText.style.textAlign = "right";
-    usrText.style.fontSize = parseInt(usrText.style.fontSize) + 2 + "pt";
+    console.log(txtinput.style.fontSize);
+    txtinput.style.textAlign = "right";
+    txtinput.style.fontSize = parseInt(txtinput.style.fontSize) + 2 + "pt";
+    boldit();
 
-    let usrCheck = document.getElementById("bling");
-    if (usrCheck.checked == true) {
-        usrText.style.fontWeight = "bold";
-        usrText.style.color = "green";
-        usrText.style.textDecoration = "underline";
-        usrText.style.backgroundImage = "url(https://cdn4.iconfinder.com/data/icons/dollars-cents-glyph/48/Sed-42-512.png)";
+
+    setTimeout(biggerText, 550);
+}
+
+function boldit() {
+    if (ckbboldit.checked == true) {
+        txtinput.style.fontWeight = "bold";
+        txtinput.style.color = "green";
+        txtinput.style.textDecoration = "underline";
+        txtinput.style.backgroundImage = "url(https://cdn4.iconfinder.com/data/icons/dollars-cents-glyph/48/Sed-42-512.png)";
     }
     else {
-        usrText.style.fontWeight = "inherit";
-        usrText.style.color = "inherit";
-        usrText.style.textDecoration = "inherit";
-        usrText.style.backgroundImage = "url()";
+        txtinput.style.fontWeight = "inherit";
+        txtinput.style.color = "inherit";
+        txtinput.style.textDecoration = "inherit";
+        txtinput.style.backgroundImage = "url()";
     }
-
-     setTimeout(biggerText, 550);
 }
 
 function Malkovitch() {
-    var arrayOfWords = $("#text").val().split(' ');
+    var arrayOfWords = $("#txtinput").val().split(' ');
 
 
     for (i = 0; i < arrayOfWords.length; i++) {
         if (arrayOfWords[i].length >= 5) {
-            arrayOfWords[i] = " Malkovitch";
+            arrayOfWords[i] = "Malkovitch";
         }
     }
-    text.value = arrayOfWords.join(' ');
-    //console.log(arrayOfWords.join(' '));
+    txtinput.value = arrayOfWords.join(' ');
 }
-
-function Igpay(text1) {
-    var arrayOfWords = $("#text").val().split(' ');
-    let consonant = /^[bcdfghjklmnpqrstvwxys]/gi, match, matches = [];
-
-    for (i = 0; i < arrayOfWords.length; i++) {
-        if (match = arrayOfWords[i].match(consonant)) {
-            console.log(match[1]);
-            matches.push(match[1]);
-        }
-    }
-        // while (match = consonant.exec(text1)) {
-        //     console.log(match[1]);
-        //     matches.push(match[1]);
-        // }
-
-        //console.log(matches.join());
-    }
